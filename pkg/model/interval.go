2a55d2cfc9eb86a4f9c8e5d67f6ad1555c0866c3
freekos <gazizbolat2@gmail.com>
2023-09-04 17:58:32 +0600

    fix: add tasks prop to interval-model

--- pkg/model/interval.go
+++ pkg/model/interval.go
@@ -10,8 +10,9 @@ type Interval struct {
 }
 
 type IntervalResponse struct {
-	ID        uint   `json:"id"`
-	StartAt   string `json:"start_at"`
-	StoppedAt string `json:"stopped_at"`
-	EndAt     string `json:"end_at"`
+	ID        uint           `json:"id"`
+	StartAt   string         `json:"start_at"`
+	StoppedAt string         `json:"stopped_at"`
+	EndAt     string         `json:"end_at"`
+	Tasks     []TaskResponse `json:"tasks"`
 }
