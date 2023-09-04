128be1a854e737e5e413e9b589e117ced61f93ea
freekos <gazizbolat2@gmail.com>
2023-09-04 17:59:16 +0600

    fix: add get-tasks for intervals

--- internal/routes/interval.go
+++ internal/routes/interval.go
@@ -9,15 +9,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//	@Summary		Create a new interval
-//	@Description	Create a new interval with the given start and end times
-//	@Tags			Intervals
-//	@Accept			json
-//	@Produce		json
-//	@Param			start_at	body		string					true	"Start time"	example:"2023-09-03T16:00:00Z"
-//	@Param			end_at		body		string					true	"End time"		example:"2023-09-03T17:00:00Z"
-//	@Success		200			{object}	model.IntervalResponse	"Successfully created interval"
-//	@Router			/intervals [post]
+// @Summary		Create a new interval
+// @Description	Create a new interval with the given start and end times
+// @Tags			Intervals
+// @Accept			json
+// @Produce		json
+// @Param			start_at	body		string					true	"Start time"	example:"2023-09-03T16:00:00Z"
+// @Param			end_at		body		string					true	"End time"		example:"2023-09-03T17:00:00Z"
+// @Success		200			{object}	model.IntervalResponse	"Successfully created interval"
+// @Router			/intervals [post]
 func (r *Route) CreateInterval(c *gin.Context) {
 	var body struct {
 		StartAt string `json:"start_at"`
@@ -36,20 +36,20 @@ func (r *Route) CreateInterval(c *gin.Context) {
 		return
 	}
 
-	intervalResponse := intervalToResponse(interval)
+	intervalResponse := intervalToResponse(interval, []model.TaskResponse{})
 
 	c.JSON(http.StatusOK, Response{Data: intervalResponse, Status: http.StatusOK, Error: nil})
 }
 
-//	@Summary		Stop an interval by ID
-//	@Description	Stop an interval by its ID
-//	@Tags			Intervals
-//	@Accept			json
-//	@Produce		json
-//	@Param			id			path		int						true	"Interval ID"
-//	@Param			stopped_at	body		string					true	"Stop time"	example:"2023-09-03T16:30:00Z"
-//	@Success		200			{object}	model.IntervalResponse	"Successfully stopped interval"
-//	@Router			/intervals/{id} [put]
+// @Summary		Stop an interval by ID
+// @Description	Stop an interval by its ID
+// @Tags			Intervals
+// @Accept			json
+// @Produce		json
+// @Param			id			path		int						true	"Interval ID"
+// @Param			stopped_at	body		string					true	"Stop time"	example:"2023-09-03T16:30:00Z"
+// @Success		200			{object}	model.IntervalResponse	"Successfully stopped interval"
+// @Router			/intervals/{id} [put]
 func (r *Route) StopInterval(c *gin.Context) {
 	id := c.Param("id")
 	var body struct {
@@ -75,18 +75,18 @@ func (r *Route) StopInterval(c *gin.Context) {
 		return
 	}
 
-	intervalResponse := intervalToResponse(interval)
+	intervalResponse := intervalToResponse(interval, []model.TaskResponse{})
 
 	c.JSON(http.StatusOK, Response{Data: intervalResponse, Status: http.StatusOK, Error: nil})
 }
 
-//	@Summary		Get all intervals
-//	@Description	Get a list of all intervals
-//	@Tags			Intervals
-//	@Accept			json
-//	@Produce		json
-//	@Success		200	{array}	model.IntervalResponse	"List of intervals"
-//	@Router			/intervals [get]
+// @Summary		Get all intervals
+// @Description	Get a list of all intervals
+// @Tags			Intervals
+// @Accept			json
+// @Produce		json
+// @Success		200	{array}	model.IntervalResponse	"List of intervals"
+// @Router			/intervals [get]
 func (r *Route) GetIntervals(c *gin.Context) {
 	intervals, err := r.repository.GetIntervals()
 	if err != nil {
@@ -97,21 +97,32 @@ func (r *Route) GetIntervals(c *gin.Context) {
 
 	intervalResponses := make([]model.IntervalResponse, len(intervals))
 	for i, interval := range intervals {
-		intervalResponses[i] = intervalToResponse(interval)
+		tasks, err := r.repository.GetTasks(interval.ID)
+		if err != nil {
+			errorMessage := err.Error()
+			c.JSON(http.StatusInternalServerError, Response{Data: nil, Status: http.StatusInternalServerError, Error: &errorMessage})
+			return
+		}
+
+		tasksResponses := make([]model.TaskResponse, len(tasks))
+		for y, task := range tasks {
+			tasksResponses[y] = taskToResponse(task)
+		}
+		intervalResponses[i] = intervalToResponse(interval, tasksResponses)
 	}
 
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, Response{Data: intervalResponses, Status: http.StatusOK, Error: nil})
 }
 
-//	@Summary		Get an interval by ID
-//	@Description	Get an interval by its ID
-//	@Tags			Intervals
-//	@Accept			json
-//	@Produce		json
-//	@Param			id	path		int						true	"Interval ID"
-//	@Success		200	{object}	model.IntervalResponse	"Interval details"
-//	@Router			/intervals/{id} [get]
+// @Summary		Get an interval by ID
+// @Description	Get an interval by its ID
+// @Tags			Intervals
+// @Accept			json
+// @Produce		json
+// @Param			id	path		int						true	"Interval ID"
+// @Success		200	{object}	model.IntervalResponse	"Interval details"
+// @Router			/intervals/{id} [get]
 func (r *Route) GetInterval(c *gin.Context) {
 	intervalID := c.Param("id")
 	fmt.Println(intervalID)
@@ -128,18 +139,29 @@ func (r *Route) GetInterval(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, Response{Data: nil, Status: http.StatusInternalServerError, Error: &errorMessage})
 		return
 	}
+	tasks, err := r.repository.GetTasks(interval.ID)
+	if err != nil {
+		errorMessage := err.Error()
+		c.JSON(http.StatusInternalServerError, Response{Data: nil, Status: http.StatusInternalServerError, Error: &errorMessage})
+		return
+	}
+	tasksResponses := make([]model.TaskResponse, len(tasks))
+	for i, task := range tasks {
+		tasksResponses[i] = taskToResponse(task)
+	}
 
-	intervalResponse := intervalToResponse(interval)
+	intervalResponse := intervalToResponse(interval, tasksResponses)
 
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, Response{Data: intervalResponse, Status: http.StatusOK, Error: nil})
 }
 
-func intervalToResponse(interval model.Interval) model.IntervalResponse {
+func intervalToResponse(interval model.Interval, tasks []model.TaskResponse) model.IntervalResponse {
 	return model.IntervalResponse{
 		ID:        interval.ID,
 		StartAt:   interval.StartAt,
 		StoppedAt: interval.StoppedAt,
 		EndAt:     interval.EndAt,
+		Tasks:     tasks,
 	}
 }
